d11e69a746e36da3e78c536e36d38ad555ecddc1
PearInJurassic <52654970+PearInJurassic@users.noreply.github.com>
2021-11-07 14:44:42 +0800

    change the searchBar to the FirstFragment

--- app/src/main/java/com/example/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,6 +19,8 @@ import com.example.myapplication.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    String [] searchItems;
+    ArrayAdapter<String> stringArrayAdapter;
 
     @Override
     public View onCreateView(
@@ -29,7 +35,30 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        // SearchView
+        binding.searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (!TextUtils.isEmpty(newText)) {
+                    binding.searchListView.setFilterText(newText);
+                } else {
+                    binding.searchListView.clearTextFilter();
+                }
+                return false;
+            }
+        });
+        // ListView
+        searchItems = getResources().getStringArray(R.array.testValues);
+        stringArrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, searchItems);
+        binding.searchListView.setAdapter(stringArrayAdapter);
+        binding.searchListView.setTextFilterEnabled(true);
+
+        // button
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             private static final String TAG = "my android debug";
 
